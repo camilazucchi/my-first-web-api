1b4c8d04a40b433c459ea619518da84ff060234f
camilazci <camilazucchi@outlook.com>
2024-05-03 16:04:49 -0300

    feat: add project

---
+++ src/test/java/com/zucchicamila/myfirstwebapi/MyFirstWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zucchicamila.myfirstwebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
