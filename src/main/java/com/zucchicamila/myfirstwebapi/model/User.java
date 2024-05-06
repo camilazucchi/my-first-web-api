d075f7527d1cd9c4aa073e27d184e87f3f081c91
camilazci <camilazucchi@outlook.com>
2024-05-06 14:16:39 -0300

    feat: add getters and setters to User entity

--- src/main/java/com/zucchicamila/myfirstwebapi/model/User.java
+++ src/main/java/com/zucchicamila/myfirstwebapi/model/User.java
@@ -20,4 +20,28 @@ public class User {
                 '}';
     }
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
