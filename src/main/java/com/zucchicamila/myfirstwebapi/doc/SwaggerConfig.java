72d5e5e58b85616e1bc543ecdcea093f97fc5d23
camilazci <camilazucchi@outlook.com>
2024-05-08 10:21:01 -0300

    chore: update SwaggerConfig for API documentation

--- src/main/java/com/zucchicamila/myfirstwebapi/doc/SwaggerConfig.java
+++ src/main/java/com/zucchicamila/myfirstwebapi/doc/SwaggerConfig.java
@@ -1,6 +1,9 @@
 package com.zucchicamila.myfirstwebapi.doc;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -9,6 +12,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SwaggerConfig implements WebMvcConfigurer {
+
+    @Value("${springdoc.swagger-ui.title}")
+    private String swaggerUiTitle;
+
+    @Value("${springdoc.swagger-ui.description}")
+    private String swaggerUiDescription;
+
+    @Value("${springdoc.version}")
+    private String version;
 
     @Bean
     public GroupedOpenApi api() {
@@ -23,6 +35,15 @@ public class SwaggerConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/swagger-ui/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/springdoc-openapi-ui/")
                 .resourceChain(false);
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title(swaggerUiTitle)
+                        .description(swaggerUiDescription)
+                        .version(version));
     }
 
 }
